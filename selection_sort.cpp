ae747f0ae7c171531da1dc702b83ab009a5c6ec4
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2025-10-17 06:40:51 +0000

    selection_sort.cpp: add checkIFSmallerAlphabetically()

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -14,6 +14,37 @@
 
 using namespace std;
 
+bool checkIfSmallerAlphabetically(const std::string& str1, const std::string& str2) {
+    for (size_t i = 0; i < str1.size(), i < str2.size(); i++) {
+        if (
+            static_cast<unsigned int>(tolower(str1[i])) < // smaller
+            static_cast<unsigned int>(tolower(str2[i]))
+        ) {
+            return true; // arg1 is smaller alphabetically
+        } else if (
+            static_cast<unsigned int>(tolower(str1[i])) > 
+            static_cast<unsigned int>(tolower(str2[i])) // smaller
+        ) {
+            return false; // arg2 is smaller alphabetically
+        }
+
+        if (str1.size() > i && str2.size() > i) {
+            // continue so program doesn't run unnecessary size checks
+            continue;
+        }
+
+        if (str1.size() < str2.size()) {
+            return 0; // str1 is smaller alphabetically
+        }
+
+        if (str1.size() > str2.size()) {
+            return 1; // str2 is smaller alphabetically
+        }
+    }
+
+    return false;
+}
+
 void selectionSort(vector<personType>& entries, const char sortKey) {
     switch (sortKey) {
         case 'a': {
